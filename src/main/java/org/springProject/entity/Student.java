2c643131203faee4b2b35507c31aa47fac092ec3
Vinit Bhosale <vinitbhosale925@gmail.com>
2021-09-01 10:02:01 -0700

    Implemented Add Student feature

--- src/main/java/org/springProject/entity/Student.java
+++ src/main/java/org/springProject/entity/Student.java
@@ -28,9 +28,8 @@ public class Student {
 	public Student() {
 		super();
 	}
-	public Student(long id, String firstName, String lastName, String email) {
+	public Student(String firstName, String lastName, String email) {
 		super();
-		this.id = id;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
